a1aaff92c58d25b9b610538563c5df8c4a5d5b18
Marcos Peralta <mrcperalta.mp@gmail.com>
2019-11-16 13:15:26 -0300

    Se eliminan clases innecesarias y cosas del siteConfig

--- mysite/code/extensions/CustomSiteConfig.php
+++ mysite/code/extensions/CustomSiteConfig.php
@@ -15,12 +15,12 @@ class CustomSiteConfig extends DataExtension {
 
     public function updateCMSFields(FieldList $fields) {
       $fields->addFieldToTab('Root.Main', new TextField('EmailContacto','Emails de contacto (separados por coma)'));
-      $fields->addFieldToTab('Root.Main', new TextField('TelefonoContacto','Número de contacto para Whatsapp Business (Formato requerido: 595991900700)'));
+      /* $fields->addFieldToTab('Root.Main', new TextField('TelefonoContacto','Número de contacto para Whatsapp Business (Formato requerido: 595991900700)'));
       $fields->addFieldToTab('Root.Main', new TextareaField('MensajeWhatsapp','Mensaje por defecto que se enviará al Whatsapp Business'));
       $fields->addFieldToTab('Root.Main', new TextField('TextoCopyright','Texto de Copyright (Pie de página)'));
       $fields->addFieldToTab('Root.Main', new TextField('FacebookLink','Facebook (Página de Facebook)'));
       $fields->addFieldToTab('Root.Main', new TextField('InstagramLink','Instagram (Página de Instagram)'));
       $fields->addFieldToTab('Root.Main', new TextField('TwitterLink','Twitter (Página de Twitter)'));
-      $fields->addFieldToTab('Root.Main', new TextField('LinkedinLink','Linkedin (Página de Linkedin)'));
+      $fields->addFieldToTab('Root.Main', new TextField('LinkedinLink','Linkedin (Página de Linkedin)')); */
     }
 }
